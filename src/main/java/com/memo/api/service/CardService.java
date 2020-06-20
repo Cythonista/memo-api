781d8253d1d61b151130d7cafb0a90543fa74fba
cythonista <gnfx0514@gmail.com>
2020-06-21 01:28:13 +0900

    add service

--- src/main/java/com/memo/api/service/CardService.java
+++ src/main/java/com/memo/api/service/CardService.java
@@ -1,4 +1,11 @@
 package com.memo.api.service;
 
+import com.memo.api.domain.Card;
+import com.memo.api.domain.CardList;
+import com.memo.api.domain.CardSelector;
+
 public interface CardService {
+    CardList find(CardSelector cardSelector);
+    //CardList find(@Param("cardId") Long cardId);
+    Card get(Long cardId);
 }
